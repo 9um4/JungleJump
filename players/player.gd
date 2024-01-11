850e02e8cd09a696f7bdb61bf92fd715a9eb2b98
9um4 <sweetenpotato@outlook.com>
2024-01-12 01:42:49 +0900

    refactor: Player Movement and Animation

--- players/player.gd
+++ players/player.gd
@@ -1,65 +1,88 @@
 extends CharacterBody2D
 
-const CROUCH_SPEED: float = 100.0
-const RUN_SPEED: float = 300.0
+# DEFAULT SPEED
+const DEFAULT_RUN_SPEED: float = 100.0
+const DEFAULT_JUMP_VELOCITY: float = -300.0
 
-const DEFAULT_JUMP_VELOCITY: float = -400.0
-const CROUCHED_JUMP_VELOCITY: float = -1000.0
+# DEFAULT MULTIPLIER
+const CROUCH_RUN_MULTIPLIER: float = 0.3
+const WALK_MULTIPLIER: float = 0.7
+const CROUCH_JUMP_MULTIPLIER: float = 1.5
+
+# DEFAULT MAX CASTING TIME
+const MAX_CROUCH_CHARGING_TIME: float = 1
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# state variables
+var canHighJump: bool = false
+var currentHorizontalMultiplier: float = 1.0
+var currentVerticalMultiplier: float = 1.0
+var currentCrouchChargingTime: float = 0.0
 
 # child nodes
 @onready var sprite: AnimationPlayer = get_node("Sprite")
 @onready var rawSprite: AnimatedSprite2D  = get_node("RawSprite")
 
-
 func _physics_process(delta):
-	if velocity.length() == 0:
-		if Input.is_action_pressed("ui_down"):
-			sprite.play("crouch")
-			rawSprite.modulate.r += delta * 10
-			rawSprite.modulate.g += delta * 10
-			rawSprite.modulate.b += delta * 10
-			
-			rawSprite.modulate.r = min(100.0, rawSprite.modulate.r)
-			rawSprite.modulate.g = min(100.0, rawSprite.modulate.g)
-			rawSprite.modulate.b = min(100.0, rawSprite.modulate.b)
+	var direction: int = Input.get_axis("ui_left", "ui_right")
+	if direction > 0:
+		rawSprite.flip_h = false
+	elif direction < 0:
+		rawSprite.flip_h = true
+	
+	if is_on_floor():
+		velocity.y = 0
+		if velocity.x != 0:
+			sprite.play("run")
 		else:
-			rawSprite.modulate.r = 1.0
-			rawSprite.modulate.g = 1.0
-			rawSprite.modulate.b = 1.0
 			sprite.play("idle")
 	else:
-		rawSprite.modulate.r = 1.0
-		rawSprite.modulate.g = 1.0
-		rawSprite.modulate.b = 1.0
-	
-	if velocity.x < 0: # facing left
-		rawSprite.flip_h = true
-	elif velocity.x > 0: # facing right
-		rawSprite.flip_h = false
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if velocity.y < 0: # jumping
-			sprite.play("jump")
-		elif velocity.y > 0: # falling
+		velocity.y += gravity * delta;
+		if velocity.y > 0:
 			sprite.play("fall")
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = DEFAULT_JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * RUN_SPEED
-		if is_on_floor():
-			sprite.play("run")
+		elif velocity.y < 0:
+			sprite.play("jump")
+			
+	# Run Multiplier
+	if Input.is_action_pressed("ui_down"):
+		currentHorizontalMultiplier = CROUCH_RUN_MULTIPLIER
+	elif Input.is_action_pressed("control_dash"):
+		currentHorizontalMultiplier = WALK_MULTIPLIER
 	else:
-		velocity.x = move_toward(velocity.x, 0, RUN_SPEED)
+		currentHorizontalMultiplier = 1.0
+		
+	if Input.is_action_pressed("ui_down") and is_on_floor():
+		sprite.play("crouch")
+		if !canHighJump:
+			currentCrouchChargingTime += delta
+			if currentCrouchChargingTime >= MAX_CROUCH_CHARGING_TIME:
+				currentCrouchChargingTime = MAX_CROUCH_CHARGING_TIME
+				canHighJump = true
+				currentVerticalMultiplier *= CROUCH_JUMP_MULTIPLIER
+				rawSprite.modulate.r8 = 0
+				
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y += DEFAULT_JUMP_VELOCITY * currentVerticalMultiplier
+		sprite.play("jump")
+		if canHighJump:
+			currentVerticalMultiplier /= CROUCH_JUMP_MULTIPLIER
+			canHighJump = false
+		currentCrouchChargingTime = 0.0
+		rawSprite.modulate.r8 = 255
+
+	if !Input.is_action_pressed("ui_down"):
+		currentCrouchChargingTime = 0.0
+		if canHighJump:
+			canHighJump = false
+			currentVerticalMultiplier /= CROUCH_JUMP_MULTIPLIER
+		rawSprite.modulate.r8 = 255
+		
+	if Input.is_action_just_released("ui_down"):
+		currentHorizontalMultiplier /= CROUCH_RUN_MULTIPLIER
+		
+	velocity.x = direction * DEFAULT_RUN_SPEED * currentHorizontalMultiplier
 
 	move_and_slide()
